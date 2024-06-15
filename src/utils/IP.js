65ac6c09e77376c4ecc37183907e412daa79b35e
XTDV2021 <xuanthudoanvo@gmail.com>
2024-06-15 16:09:18 +0700

    first commit

--- src/utils/IP.js
+++ src/utils/IP.js
@@ -1,4 +1,3 @@
-const IP = "192.168.101.25"
-const PORT = "5000"
-export const baseUrl = `http://${IP}:${PORT}/api/v1`
-
+const IP = "192.168.1.3";
+const PORT = "5000";
+export const baseUrl = `http://${IP}:${PORT}/api/v1`;
